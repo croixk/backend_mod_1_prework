c2efa5aa2dbaaa339201db2d2238b89d2984d4bd
Croix Kimmel <croixkimmel@gmail.com>
2021-09-21 15:42:09 -0600

    Update mod_zero_hero.rb

--- final_prep/mod_zero_hero.rb
+++ final_prep/mod_zero_hero.rb
@@ -12,43 +12,81 @@ special_ability = 'flying'
 greeting = 'hello, this is #{hero_name}'
 catchphrase = '#{special_ability} to the rescue!'
 
-
 # Declare two variables - power AND energy - set to integers
 
-power
+power = 10 
+energy = 10 
 
 # Declare two variables - full_power AND full_energy
 #   full_power should multiply your current power by 500
 #   full_energy should add 150 to your current energy
 
+full_power = power * 500 
+full_energy = energy + 150 
+
 # Declare two variables - is_human and identity_concealed - assigned to booleans
 
+is_human = true 
+identity_concealed = false
 
 # Declare two variables - arch_enemies AND sidekicks
 #   arch_enemies should be an array of at least 3 different enemy strings
 #   sidekicks should be an array of at least 3 different sidekick strings
 
+arch_enemies = ['joe', 'bob', 'sue'] 
+sidekicks = ['jill', 'keith', 'stewart']
+
 # Print the first sidekick to your terminal
+
+print sidekicks(0)
 
 # Print the last arch_enemy to the terminal
 
+print arch_enemies(2)
+
 # Write some code to add a new arch_enemy to the arch_enemies array
+
+
 
 # Print the arch_enemies array to terminal to ensure you added a new arch_enemey
 
+print arch_enemies 
+
 # Remove the first sidekick from the sidekicks array
 
+
+
+
 # Print the sidekicks array to terminal to ensure you added a new sidekick
+
+print sidekicks 
 
 # Create a function called assess_situation that takes three arguments - danger_level, save_the_day, bad_excuse
 #   - danger_level should be an integer
 #   - save_the_day should be a string a hero would say once they save the day 
 #   - bad_excuse should be a string a hero would say if they are too afraid of the danger_level
 
+def assess_situation (danger_level, save_the_day, bad_excuse)
+
+if danger_level > 50 
+  puts bad_excuse 
+  
+elsif danger_level >= 10 
+  puts save_the_day
+    
+else 
+  'Meh. Hard pass'
+  
+end 
+
+
 # Your function should include an if/else statement that meets the following criteria
 #   - Danger levels that are above 50 are too scary for your hero. Any danger level that is above 50 should result in printing the bad_excuse to the terminal
 #   - Anything danger_level that is between 10 and 50 should result in printing the save_the_day string to the terminal
 #   - If the danger_level is below 10, it means it is not worth your time and should result in printing the string "Meh. Hard pass." to the terminal.
+
+
+
 
 #Test Cases
 announcement = 'Never fear, the Courageous Curly Bracket is here!'
@@ -65,6 +103,9 @@ excuse = 'I think I forgot to lock up my 1992 Toyota Coralla. Be right back.'
 #   - luckyNumbers (array)
 #   - address (hash with following key/values: number , street , state, zip)
 
+scary_monster = {name => 'gus', smell => 'bad', weight => 150, citiesDestroyed = [], luckyNumbers = [], address = {number => 50, 
+  street => '', state => '', zip => 50220}
+  
 
 # Create a new class called SuperHero
 # - Your class should have the following DYNAMIC values
@@ -76,13 +117,35 @@ excuse = 'I think I forgot to lock up my 1992 Toyota Coralla. Be right back.'
 #   - power_level = 100
 #   - energy_level = 50 
 
+Class Superhero 
+attr_reader :name, :super_power, :age
+  
+  arch_nemesis = "The Syntax Error"
+  power_level = 100 
+  energy_level = 50
+  
+  def say_name 
+    puts name 
+  end 
+  
+  def maximize_energy 
+    energy_level = 100 
+  end 
+  
+  def gain_power(number) 
+    power_level = power_level + 100 
+  end 
+  
+end 
+
 # - Create the following class methods
 #   - say_name, should print the hero's name to the terminal
 #   - maximize_energy, should update the energy_level to 1000
 #   - gain_power, should take an argument of a number and INCREASE the power_level by that number
 
 # - Create 2 instances of your SuperHero class
-
+instance1 = SuperHero.new
+instance2 = SuperHero.new
 
 # Reflection
 # What parts were most difficult about this exerise?
