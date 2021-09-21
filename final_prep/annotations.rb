03f29923b6ef55df73f7fd7c33d5264c831cc3d6
Croix Kimmel <croixkimmel@gmail.com>
2021-09-21 16:21:48 -0600

    Update annotations.rb

--- final_prep/annotations.rb
+++ final_prep/annotations.rb
@@ -4,10 +4,15 @@
 
 # Build a Bear
 
+# define method - establish arguments 
 def build_a_bear(name, age, fur, clothes, special_power)
+  # Use code 
   greeting = "Hey partner! My name is #{name} - will you be my friend?!"
+  # array - includes name and age of bear 
   demographics = [name, age]
+  # expression that bear says 
   power_saying = "Did you know that I can #{special_power}?"
+  # Hash for bear characteristics - keys include basic info, clothes, exterior, cost, sayings, and whether it is cuddly
   built_bear = {
     'basic_info' => demographics,
     'clothes' => clothes,
@@ -16,10 +21,13 @@ def build_a_bear(name, age, fur, clothes, special_power)
     'sayings' => [greeting, power_saying, "Goodnight my friend!"],
     'is_cuddly' => true,
   }
+  # explicit return for method - returns hash
   return built_bear
 end
 
+# first build a bear, with traits for bear included in arguments
 build_a_bear('Fluffy', 4, 'brown', ['pants', 'jorts', 'tanktop'], 'give you nightmares')
+# second build a bear
 build_a_bear('Sleepy', 2, 'purple', ['pajamas', 'sleeping cap'], 'sleeping in')
 
 
